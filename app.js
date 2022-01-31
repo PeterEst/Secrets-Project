84c61f45464d7bbc6377073a82809064961008ee
PeterEst <peter-estephan@outlook.com>
2022-01-31 13:20:16 +0200

    Removed Secret Variable

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req,res)=>{
